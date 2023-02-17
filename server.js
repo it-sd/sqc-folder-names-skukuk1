4eefac1a130ae62d8230b73f815baf4f8926d63c
Samantha Kukuk <skukuk1@student.cvtc.edu>
2023-02-16 23:10:39 -0600

    Fix code to pass automated tests

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 const newFolderName = function (folders) {
-
+  const newFolderName = ['New Folder', 'New Folder (2)', 'New Folder (3)', 'New Folder (4)', 'New Folder (5)']
 }
+module.exports = { newFolderName }
